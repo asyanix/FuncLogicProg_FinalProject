8f285cb01a3bc0f9bbab326054f72d7288cee067
Asya <111695385+asyanix@users.noreply.github.com>
2025-05-24 19:39:43 +0300

    Task 1: Creating an imperative solution for task 1167C about ‘News Distribution’.

---
+++ News distribution/NewsCSharp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    const int N = 1000043; 
+    static List<int>[] g = new List<int>[N];  // Список смежности (граф) (g[i] хранит список вершин, смежных с вершиной i)
+    static int[] compontent = new int[N];     // Номера компонентов связности вершин (изначально для всех i component[i]=0)
+    static int[] siz = new int[N];            // Размеы компоненты связности с номером i
+    static int n, m;                          // Количество пользователей, количество групп
+    static int cc = 0;                        // Счётчик компонентов связности
+
+    static int DFS(int x)
+    {
+        if (compontent[x] != 0)
+            return 0;
+
+        compontent[x] = cc;
+        int ans = (x < n ? 1 : 0);            // ans — количество людей в компоненте
+
+        foreach (var y in g[x])
+        {
+            ans += DFS(y);                    // Смотрим всех соседей y текущей вершины x
+        }
+
+        return ans;
+    }
+
+    static void Main()
+    {
+        string[] firstLine = Console.ReadLine().Split();
+        n = int.Parse(firstLine[0]);
+        m = int.Parse(firstLine[1]);
+
+        // Инициализируем списки смежности
+        for (int i = 0; i < n + m; i++)
+        {
+            g[i] = new List<int>();
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            string[] line = Console.ReadLine().Split();
+            int k = int.Parse(line[0]);
+            
+            // Создаем связи
+            for (int j = 0; j < k; j++)
+            {
+                int x = int.Parse(line[j + 1]) - 1;
+                g[x].Add(i + n);              // x <-> группа
+                g[i + n].Add(x);              // группа <-> x
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (compontent[i] == 0)           // Если вершина не обработана, запускаем DFS
+            {
+                cc++;
+                siz[cc] = DFS(i);
+            }
+
+            Console.Write(siz[compontent[i]] + " ");
+        }
+    }
+}
