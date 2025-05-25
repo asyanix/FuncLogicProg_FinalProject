4a93f12cc4bf633b3383bcdfc23a42b3052f8917
Darya <darya.pnmr@gmail.com>
2025-05-25 18:59:09 +0300

    Реализация решения на C# для задачи 115А 'Праздник'

---
+++ Holiday/HolidayCSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] p = new int[n + 1]; 
+        string[] input = Console.ReadLine().Split();
+
+        for (int i = 1; i <= n; i++)
+            p[i] = int.Parse(input[i - 1]);
+
+        int ans = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            int t = p[i];
+            int c = 1;
+            while (t != -1)
+            {
+                c++;
+                t = p[t];
+            }
+            ans = Math.Max(ans, c);
+        }
+
+        Console.WriteLine(ans);
+    }
+}
